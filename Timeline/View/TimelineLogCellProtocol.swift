d436d61fe4d8759fe5f79fa58c88f796eb39aeea
Jongwon Leee <jw.leee@linecorp.com>
2021-12-08 01:15:27 +0900

    implement TimelineMemoLogViewCell

--- Timeline/View/TimelineLogCellProtocol.swift
+++ Timeline/View/TimelineLogCellProtocol.swift
@@ -44,12 +44,62 @@ class TimelineDefaultLogViewCell: UICollectionViewCell {
     }
 }
 
-class TimelineMemoLogViewCell: UICollectionViewCell, TimelineLogCellProtocol {
+class TimelineMemoLogViewCell: TimelineDefaultLogViewCell, TimelineLogCellProtocol {
     struct Layout {
         static let height = CGFloat(70)
+        static let labelMargin: CGFloat = 10
     }
 
     static var reuseIdentifier = "TimelineMemoLogViewCell"
+
+    private let memoLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.numberOfLines = 4
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+
+    var constraintsLayout: [NSLayoutConstraint] = []
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(memoLabel)
+
+        NSLayoutConstraint.activate([
+            memoLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        assertionFailure("wrong initalizer")
+    }
+
+    func configure(viewModel: TimelineLogViewModel) {
+        guard let viewModel = viewModel as? TimelineMemoLogViewModel else { return }
+        memoLabel.text = viewModel.memo
+
+        NSLayoutConstraint.deactivate(constraintsLayout)
+        switch viewModel.location {
+        case .left:
+            memoLabel.textAlignment = .left
+            constraintsLayout = [
+                memoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Layout.labelMargin),
+                memoLabel.trailingAnchor.constraint(equalTo: centerDotView.leadingAnchor, constant: -Layout.labelMargin),
+            ]
+        case .right:
+            memoLabel.textAlignment = .right
+            constraintsLayout = [
+                memoLabel.leadingAnchor.constraint(equalTo: centerDotView.trailingAnchor, constant: Layout.labelMargin),
+                memoLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Layout.labelMargin),
+            ]
+        }
+        NSLayoutConstraint.activate(constraintsLayout)
+        setNeedsLayout()
+    }
 }
 
 class TimelineImageLogViewCell: UICollectionViewCell, TimelineLogCellProtocol {
